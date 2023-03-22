f265c7d5265bcc76eba520aeeb2ef26bae0f2c49
alvigabriel <claudio.alves20112011@hotmail.com>
2023-03-22 11:28:46 -0300

    Criando método, criando validações get e set

--- Intermediário/Models/Pessoa.cs
+++ Intermediário/Models/Pessoa.cs
@@ -7,7 +7,33 @@ namespace Intermediário.Models
 {
     public class Pessoa
     {
-        public string Nome { get; set; } //essas são propriedades
-        public int Idade { get; set; }
+        private string _nome; // campo criado para encapsulamento (proteção, validação de métodos)
+
+
+        public string Nome 
+        { 
+            get 
+            {
+                return _nome.ToUpper();
+            } 
+            set // ao atribuir um valor, passe a condicional...
+            {
+               if(value == "") 
+               {
+                    throw new ArgumentException("O nome não pode ser vazio");
+               }
+               _nome = value;
+            }
+        } //essas são propriedades
+        public int Idade 
+        { 
+            get; 
+            set;
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
     }
 }
--- Intermediário/Program.cs
+++ Intermediário/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Olá, mundo");
+﻿using Intermediário.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "";
+p1.Idade = 26;
+p1.Apresentar();
+
