21361df3422d3e928f2ee96efd56dfc3f59eddfb
alvigabriel <claudio.alves20112011@hotmail.com>
2023-03-23 08:49:50 -0300

    Formatando valores monetários

--- Intermediário/Program.cs
+++ Intermediário/Program.cs
@@ -1,14 +1,23 @@
 ﻿using Intermediário.Models;
 
-Pessoa pessoa1 = new Pessoa(nome: "Claudio", sobrenome: "Nascimento");
+// Pessoa pessoa1 = new Pessoa(nome: "Claudio", sobrenome: "Nascimento");
 
-Pessoa pessoa2 = new Pessoa(nome: "Gabrie", sobrenome: "Alvi");
+// Pessoa pessoa2 = new Pessoa(nome: "Gabrie", sobrenome: "Alvi");
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.addAluno(pessoa1);
-cursoDeIngles.addAluno(pessoa2);
-cursoDeIngles.ListarAlunos();
+// cursoDeIngles.addAluno(pessoa1);
+// cursoDeIngles.addAluno(pessoa2);
+// cursoDeIngles.ListarAlunos();
+
+// FORMATANDO VALORES MONETÁRIOS =================================
+
+decimal valorMonetario = 1582.40M;
+
+
+// : significa que é desejado formatar uma string . C significa Currency
+Console.WriteLine($"{valorMonetario:C}");
+
 
