5258ef08e3b89c5d54ff0ddff2a948c1c18f2582
alvigabriel <claudio.alves20112011@hotmail.com>
2023-03-25 12:01:14 -0300

    Throw

--- Exceções/Models/Ex.cs
+++ Exceções/Models/Ex.cs
@@ -8,5 +8,23 @@ namespace Exceções.Models
     public class Ex
     {
         
+        public void Metodo1()
+        {
+            Metodo2();
+        }
+        public void Metodo2()
+        {
+            Metodo3();
+        }
+        public void Metodo3()
+        {
+            Metodo4();
+        }
+        public void Metodo4()
+        {
+            // trow joga o erro para outro bloco de código e para ser tratado
+            throw new Exception("Ocorreu uma exceção");
+        }
     }
+
 }
--- Exceções/Program.cs
+++ Exceções/Program.cs
@@ -1,23 +1,25 @@
-﻿
+﻿using Exceções.Models;
 
-try
-{
-// A classe File lida com arquivos, read lê um arquivo
-    string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
+// try
+// {
+// // A classe File lida com arquivos, read lê um arquivo
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivo_Leitura.txt");
 
-    foreach(string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
+//     foreach(string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
 
-} catch(FileNotFoundException ex)
-{
-        Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado\n {ex.Message}");   
-} catch(Exception ex)
-{
-        Console.WriteLine($"Ocorreu uma exceção genérica\n {ex.Message}");   
-} finally //executa um bloco de código independente se ocorreu erro ou não
-{
-        Console.WriteLine("Chegou até aqui");   
+// } catch(FileNotFoundException ex)
+// {
+//         Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado\n {ex.Message}");   
+// } catch(Exception ex)
+// {
+//         Console.WriteLine($"Ocorreu uma exceção genérica\n {ex.Message}");   
+// } finally //executa um bloco de código independente se ocorreu erro ou não
+// {
+//         Console.WriteLine("Chegou até aqui");   
     
-}
+// }
+
+new Ex().Metodo1(); 
