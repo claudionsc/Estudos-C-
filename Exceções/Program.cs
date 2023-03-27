1b8306bc73384652a6c9d56cf087d7be0ea36d62
alvigabriel <claudio.alves20112011@hotmail.com>
2023-03-27 11:17:52 -0300

    Trabalhando com Dictionary no C#

--- Exceções/Program.cs
+++ Exceções/Program.cs
@@ -51,25 +51,72 @@
 
 // PILHAS =======================================================
 
-Stack<int> pilha = new Stack<int>();
+// Stack<int> pilha = new Stack<int>();
 
-// ADD NA PILHA
+// // ADD NA PILHA
 
-pilha.Push(4);
-pilha.Push(6);
-pilha.Push(8);
-pilha.Push(10);
+// pilha.Push(4);
+// pilha.Push(6);
+// pilha.Push(8);
+// pilha.Push(10);
 
-foreach(int item in pilha)
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// //  REMOVER DA PILHA -  REMOVE SEMPRE O ÚLTIMO DA PILHA
+
+// Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+// foreach(int item in pilha)
+// {
+//     Console.WriteLine(item);
+// }
+
+// DICTIONARY - COLEÇÃO CHAVEADA COM APENAS ELEMENTOS ÚNICOS ===================== 
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+// ADD NO DICTIONARY
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+// Percorrendo o dictionary
+
+foreach(var item in estados)
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
 }
 
-//  REMOVER DA PILHA -  REMOVE SEMPRE O ÚLTIMO DA PILHA
+// REMOVENDO/ALTERANDO ELEMENTOS DO DICTIONARY - OBS: SÓ É POSSÍVEL ALTERAR O VALOR, ENÃO A CHAVE
 
-Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+estados.Remove("SP");
 
-foreach(int item in pilha)
+Console.WriteLine("===========================");
+
+estados["MG"] = "Minas -  valor alterado";
+
+foreach(var item in estados)
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
 }
+
+// VERIFICANDO SE JÁ EXISTE CHAVE 
+
+string chave = "BA2";
+Console.WriteLine($"Verificando o elemento: {chave}");
+
+if (estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente: {chave}");
+} 
+else 
+{
+    Console.WriteLine($"Valor não existe. É seguro adicionar a chave: {chave}");
+}
+
+// Retornar valor existente no dictionary
+
+Console.WriteLine($"Estado: {estados["MG"]}");
