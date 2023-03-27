df38c4994bd3e60454fccdc1b6cee5a97bcc7fe6
alvigabriel <claudio.alves20112011@hotmail.com>
2023-03-27 11:00:37 -0300

    Stack -  pilhas com C#

--- Exceções/Program.cs
+++ Exceções/Program.cs
@@ -26,25 +26,50 @@
 
 // FILAS =======================================================
 
-Queue<int> fila = new Queue<int>();
+// Queue<int> fila = new Queue<int>();
 
-// ADD NA FILA
+// // ADD NA FILA
 
-fila.Enqueue(2);
-fila.Enqueue(4);
-fila.Enqueue(6);
-fila.Enqueue(8);
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
 
-foreach(int item in fila)
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// // REMOVER DA FILA -  REMOVE SEMPRE O PRIMEIRO ELEMENTO DA FILA
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+
+// foreach(int item in fila)
+// {
+//     Console.WriteLine(item);
+// }
+
+// PILHAS =======================================================
+
+Stack<int> pilha = new Stack<int>();
+
+// ADD NA PILHA
+
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach(int item in pilha)
 {
     Console.WriteLine(item);
 }
 
-// REMOVER DA FILA -  REMOVE SEMPRE O PRIMEIRO ELEMENTO DA FILA
+//  REMOVER DA PILHA -  REMOVE SEMPRE O ÚLTIMO DA PILHA
 
-Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
 
-foreach(int item in fila)
+foreach(int item in pilha)
 {
     Console.WriteLine(item);
 }
